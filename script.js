4232b3130da6cc071d3b69450fcec4e5a5bc260f
Rishi731 <86367086+Rishi731@users.noreply.github.com>
2022-03-18 21:13:16 +0530

    Saved!

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.bi1').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.bi').style.display = 'inline';
+        document.querySelector('.bi1').style.display = 'none';
+    }
+    else{
+        document.querySelector('.bi').style.display = 'none';
+        setTimeout(()=>{
+            document.querySelector('.bi1').style.display = 'inline';
+        }, 300);
+    }
+})
